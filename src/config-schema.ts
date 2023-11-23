1039b86e79900406c034d1b9bf86970930010ea9
Michael Ndula <michaelndula@gmail.com>
2023-11-23 12:08:59 +0300

    publish the scope package

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -1,5 +1,16 @@
-import { Type, validator } from "@openmrs/esm-framework";
+import { Type } from "@openmrs/esm-framework";
 
-export const configSchema = {};
+export const configSchema = {
+  cd4ViralLoadConcepts: {
+    _type: Type.Array,
+    _default: [
+      "809dd0f5-ce54-441c-b835-a2a8b06a6140",
+      "01c3ce55-b7eb-45f5-93d5-bace353e3cfd",
+    ],
+    _description: "List of concept uuids for CD4 and Viral Load",
+  },
+};
 
-export type Config = {};
+export interface ConfigObject {
+  cd4ViralLoadConcepts: Array<string>;
+}
