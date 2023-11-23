4dbd78b719192e1270591c54d27784731903460a
Michael Ndula <michaelndula@gmail.com>
2023-11-23 12:12:48 +0300

    publish the scope package

--- src/dashboard/reports-dashboard.component.tsx
+++ src/dashboard/reports-dashboard.component.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { ExtensionSlot } from '@openmrs/esm-framework';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { ExtensionSlot } from "@openmrs/esm-framework";
 
 const ReportsDashboard: React.FC = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    React.useEffect(() => {
-        const redirectUrl = 'https://ssemr.intellisoftkenya.com/openmrs/module/reporting/dashboard/index.form';
+  React.useEffect(() => {
+    const redirectUrl =
+      "https://ssemr.intellisoftkenya.com/openmrs/module/reporting/dashboard/index.form";
 
-        navigate(redirectUrl);
-    }, []);
+    navigate(redirectUrl);
+  }, []);
 
-    return null;
+  return null;
 };
 
 export default ReportsDashboard;
