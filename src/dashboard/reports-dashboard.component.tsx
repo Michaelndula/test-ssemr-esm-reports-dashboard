5a6701757c592f547b5a3820bc1953bb930a1615
Michael Ndula <michaelndula@gmail.com>
2023-11-23 12:14:17 +0300

    publish the scope package

--- src/dashboard/reports-dashboard.component.tsx
+++ src/dashboard/reports-dashboard.component.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { ExtensionSlot } from "@openmrs/esm-framework";
 
 const ReportsDashboard: React.FC = () => {
   const navigate = useNavigate();
